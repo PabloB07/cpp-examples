081cfbd5b961842b210a3b6c24b4f2008b64d03a
PabloB07 <frontgames2016@outlook.com>
2019-05-26 04:22:52 -0400

    Initial commit

---
+++ resultado.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int num1, num2;
+
+int main()
+{
+    cout << "Coloca un numero\n " << endl;
+    cin >> num1;
+    cout << "Coloca otro numero\n " << endl;
+    cin >> num2;
+    cout << "El resultado es: " << num1 + num2;
+    return 0;
+}
